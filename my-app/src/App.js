2eebbed19b2790f8e05377ba21305d8638a7f4fe
Brenda Berenice Ramos Hernández <elcosmossuena@gmail.com>
2019-12-21 09:35:25 -0600

    se actualizo npm y se instalo correctamente redux

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
-import Home from './Views/Home/Home'
-import Search from './Views/View-Data/Search'
+import Home from './views/Home/Home'
+import Search from './views/View-Data/Search'
 
 function App() {
   return (
---
+++ my-app/src/views/View-Data/Search.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './Search.scss'
+
+import Navbar from '../Utils/Navbar/Navbar';
+// import Option from '../Utils/Select/Option';
+
+
+
+class Search extends Component {
+    render() {
+    return (
+        <section className="section-search">
+            <div>
+                <Navbar/>
+            </div>
+            
+            <div className="box-search">
+                <div>
+                    {/* <i className="fas fa-search"></i> */}
+                    <input placeholder="Buscar por año, velocidad o ángulo de la CME" className="input-search"></input>
+                </div>
+            </div>
+           
+           
+        </section>
+    )
+}}
+
+export default Search;
