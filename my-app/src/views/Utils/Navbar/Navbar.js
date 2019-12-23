e12bddb285e873268ba85be4ef9350b7f4413ec2
Brenda Berenice Ramos Hernández <elcosmossuena@gmail.com>
2019-12-23 13:17:14 -0600

    se agrego scroll a la caja de las tarjetas

--- my-app/src/views/Utils/Navbar/Navbar.js
+++ my-app/src/views/Utils/Navbar/Navbar.js
@@ -8,7 +8,6 @@ function Navbar() {
             <ul className="list-nav">
                 <lo className="item-list">INICIO</lo>
                 <lo className="item-list">BUSCAR CMEs</lo>
-                <lo className="item-list">FUENTES</lo>
             </ul>
         </nav>
     )}
