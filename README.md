f279a766ef72a8684be1229d2ce5e9dd494d6056
Brenda Berenice Ramos Hernández <elcosmossuena@gmail.com>
2019-12-23 14:06:42 -0600

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# RunaHR-Test
---
+++ precache-manifest.c2fd334a76bd5cde27985f4a64ab38fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b251f43ae2a466ead74ae273be8da009",
+    "url": "/RunaHR-Test/index.html"
+  },
+  {
+    "revision": "1ff885a8b28f52736b26",
+    "url": "/RunaHR-Test/static/css/main.9bd4ac7c.chunk.css"
+  },
+  {
+    "revision": "80368f3a9eebb4acdfd6",
+    "url": "/RunaHR-Test/static/js/2.d4ec0c45.chunk.js"
+  },
+  {
+    "revision": "0d1a1ea291563329bcf84afb677757e8",
+    "url": "/RunaHR-Test/static/js/2.d4ec0c45.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1ff885a8b28f52736b26",
+    "url": "/RunaHR-Test/static/js/main.090d6ac6.chunk.js"
+  },
+  {
+    "revision": "ef179b62a647fd372cac",
+    "url": "/RunaHR-Test/static/js/runtime-main.6ffc2344.js"
+  },
+  {
+    "revision": "0cc0a7983da2b1aad769b1469e3575eb",
+    "url": "/RunaHR-Test/static/media/cme_sun.0cc0a798.gif"
+  }
+]);
